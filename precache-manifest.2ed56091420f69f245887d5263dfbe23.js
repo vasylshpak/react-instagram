08e1afd88837fd3588763f82db66b284bd412a2c
Vasyl Shpak <vasyl.shpak4@gmail.com>
2019-10-21 21:12:15 +0200

    Updates

---
+++ precache-manifest.2ed56091420f69f245887d5263dfbe23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd90015fa71c704c3cc6e341ff28a29",
+    "url": "/vasylshpak/react-instagram/index.html"
+  },
+  {
+    "revision": "060a6380d495af994ac0",
+    "url": "/vasylshpak/react-instagram/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "b47d12df7c57d1a5e883",
+    "url": "/vasylshpak/react-instagram/static/css/main.fcc6103c.chunk.css"
+  },
+  {
+    "revision": "060a6380d495af994ac0",
+    "url": "/vasylshpak/react-instagram/static/js/2.01203540.chunk.js"
+  },
+  {
+    "revision": "b47d12df7c57d1a5e883",
+    "url": "/vasylshpak/react-instagram/static/js/main.c8b54eaf.chunk.js"
+  },
+  {
+    "revision": "074439e28af41f2d640b",
+    "url": "/vasylshpak/react-instagram/static/js/runtime-main.0e322e83.js"
+  },
+  {
+    "revision": "4563937bb19a21d1222f1758c97ef4da",
+    "url": "/vasylshpak/react-instagram/static/media/instagram-logo.4563937b.png"
+  }
+]);
